63143aa59472e9487c41f28608608d36e2ce2230
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-25 17:12:58 +0300

    Create About.sh

---
+++ Core/About.sh
@@ -0,0 +1,16 @@
+echo '
+    ┌──────────────────────────────────────────────────────────────┐
+    │  	               ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	              │
+    │  		       ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 		      │
+    │  		       ╹ ╹┗━┛┗━┛┗━┛ ╹ 		      │
+    │──────────────────────────────────────────────────────────────│
+    │                                                              │
+    │ >>   Hi, here you can see my telegram and github there       |
+    |       will be questions you will write in telegram.          │
+    │                                                              │
+    │ >>  Author   :  Misha Korzhik                                │
+    │     Github   :  https://github.com/mishakorzik               │
+    │     Telegram :  pseudonym @MishaKorzhikTelegram              │
+    │                                                              │
+    └──────────────────────────────────────────────────────────────┘ 
+'|lolcat -a -d 10 -p 1.5 -s 40.0
