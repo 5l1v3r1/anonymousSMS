27266b8738abd7e04d647c8ec7a326b6e1262096
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-25 17:05:36 +0300

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,14 @@
+clear
+apt install figlet toilet python curl ruby -y
+apt install python3-pip
+gem install lolcat
+echo "Developer: mishakorzhik"
+echo Requirements Installed...
+echo Press Enter To Continue...
+read a1
+clear
+cd
+cd
+cd anonymousSMS
+figlet -f RUSTO Anonymous-SMS | lolcat -p 0.7
+python2 Core/MainMenu.py
