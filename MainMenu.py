63e043c496c2456c628f7f0d122c648c26da78f4
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-25 17:10:58 +0300

    Create MainMenu.py

---
+++ MainMenu.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import time
+import random
+import sys
+
+os.system("clear")
+os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+
+print("  \033[1;34m[ 01 ] >> \033[1;36;40mAnonymous SMS")
+print("  \033[1;34m[ 02 ] >> \033[1;36;40mAbout Utility")
+print("  \033[1;34m[ 03 ] >> \033[1;36;40mExit Utility")
+
+op=int(raw_input("An0N-SMs: "))
+
+if(op==1):
+ os.system("clear")
+ os.system("cd && cd anonymousSMS && python3 sms.py")
+elif(op==2):
+ os.system("clear")
+ os.system("cd && cd anonymousSMS && bash Core/About.sh")
+elif(op==3):
+ print("\033[1;31;40mQuiting Utility....")
+ time.sleep(0.5)
+else:
+ print("\033[1;31;40mInvalid input. Quiting Utility") 
+ time.sleep(0.5)
+ os.system("cd")
+ os.system("cd")
