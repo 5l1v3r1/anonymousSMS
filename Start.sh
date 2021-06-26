25beb4cd08e88511b77236f37f5de67cd32076be
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-26 15:52:33 +0300

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -3,12 +3,12 @@ apt install figlet toilet python curl ruby -y
 apt install python3-pip
 gem install lolcat
 echo "Developer: mishakorzhik"
-echo Requirements Installed...
 echo Press Enter To Continue...
 read a1
 clear
 cd
 cd
-cd anonymousSMS
 figlet -f RUSTO Anonymous-SMS | lolcat -p 0.7
+cd anonymousSMS
 python2 Core/MainMenu.py
+
